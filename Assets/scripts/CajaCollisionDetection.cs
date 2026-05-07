ad0148e9fbf3945863e47cb85113284b5e2afc3b
facunhn <130949912+facunhn@users.noreply.github.com>
2026-05-07 11:41:31 -0300

    2

---
+++ Assets/scripts/CajaCollisionDetection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaCollisionDetection : MonoBehaviour
+{
+    public CajaMovement CajaMovementScript;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        CajaMovementScript = GetComponent<CajaMovement>();
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Piso"))
+        {
+            CajaMovementScript.MoveBoxToRandomPosition();
+        }
+    }
+}
+
--- Assets/scripts/CajaMovement.cs
+++ Assets/scripts/CajaMovement.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class CajaMovement : MonoBehaviour
 {
     public float velocidad = 3f;
+    public float pisoY = -5f;  // límite inferior
+    float initialYValue;
 
     void Start()
     {
-        
+        initialYValue = transform.position.y;  // guarda posición original
     }
 
     void Update()
     {
         transform.Translate(0, -velocidad * Time.deltaTime, 0);
+
+        if (transform.position.y <= pisoY)  // si llegó al piso...
+        {
+            MoveBoxToRandomPosition();       // ...vuelve al origen
+        }
+    }
+
+    public void MoveBoxToRandomPosition()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.y = initialYValue;
+        transform.position = newPosition;
     }
 }
