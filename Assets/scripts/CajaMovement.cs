897158e42d38af7f9a744c9afb059033c65a73c2
facunhn <130949912+facunhn@users.noreply.github.com>
2026-05-07 10:18:12 -0300

    Initial commit

---
+++ Assets/scripts/CajaMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaMovement : MonoBehaviour
+{
+    public float velocidad = 3f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(0, -velocidad * Time.deltaTime, 0);
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float limiteDerecha = 5f;
+    public float limiteIzquierda = -5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+    {
+        transform.Translate(2,0,0);
+    }else     if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+    {
+        transform.Translate(-2,0,0);
+    }
+     float x = Mathf.Clamp(transform.position.x, limiteIzquierda, limiteDerecha);
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+
+    }
+}
